d14c09c1f7eaa89b078f7f5e69a755492cf96678
Adnan <adnan1262a@gmail.com>
2026-09-21 02:22:33 +0300

    Problem R-0018 Solution

---
+++ Level-1/R-Age_in_Days-0018.cpp
@@ -0,0 +1,15 @@
+  #include <iostream>
+    #include<iomanip>
+    using namespace std;   
+ 
+ 
+    int main() {
+    
+        int n ; cin >> n  ; 
+        
+        cout<<n/365<<"years"<<endl;
+        n=n%365;
+        cout<<n/30<<"months"<<endl;
+        cout<<n%30<<"days";
+        
+    }
