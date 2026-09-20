56cdb21bb392dfa8c9039fbf0209e046fc7097b3
Adnan <adnan1262a@gmail.com>
2026-09-20 22:56:37 +0300

    nothing

--- Level-1/M-Capital_or_Small_or_Digit-0013.cpp
+++ Level-1/M-Capital_or_Small_or_Digit-0013.cpp
@@ -8,11 +8,11 @@
     cin>>x; 
     if(x>=48 && x<=57)
     cout<<"IS DIGIT";
-    else 
-    cout<<"ALPHA";
+    else {
+    cout<<"ALPHA"<<endl;
     if(x>=65 && x<=90)
     cout<<"IS CAPITAL";
     else cout<<"IS SMALL";
-
+    }
     
     }
