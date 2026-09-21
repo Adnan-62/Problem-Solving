ac924a4e30e8af86497d55cbf90ee0501334b650
Adnan <adnan1262a@gmail.com>
2026-09-21 19:35:08 +0300

    Problem Z-0026 Solution

---
+++ Level-1/Z-Hard_Compare-0026.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+    #include<iomanip>
+    #include <math.h>
+    using namespace std;   
+    
+    
+    int main() {
+    long long a , b, c ,d ; 
+    cin>>a>>b>>c>>d;
+    double x =b*log(a) , y=d*log(c);
+    if(x>y)
+    cout<<"YES";
+    else 
+    cout<<"NO";
+    
+        
+        }
