286ac7f6bbb4f0bc0c2883b8f7b2d2ab61fbdcfa
Adnan <adnan1262a@gmail.com>
2026-09-21 17:24:24 +0300

    Problem V-0022 Solution

---
+++ Level-1/V-Comparison-0022.cpp
@@ -0,0 +1,23 @@
+  #include <iostream>
+    #include<iomanip>
+    #include <math.h>
+    using namespace std;   
+ 
+ 
+    int main() {
+   int x , y ; 
+   char c ; cin>>x>>c>>y;
+   if(c=='>'){
+    if(x>y)
+    cout<< "Right";
+    else cout <<"Wrong";
+   }else if (c== '=' ){
+      if(x==y)
+    cout<< "Right";
+    else cout <<"Wrong";
+   }else {
+     if(x<y)
+    cout<< "Right";
+    else cout <<"Wrong";
+   }
+    }
