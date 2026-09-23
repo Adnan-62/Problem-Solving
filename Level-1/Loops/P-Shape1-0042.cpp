ff0fd15e288f5bab8593455de2f45b006f66cd2a
Adnan <adnan1262a@gmail.com>
2026-09-23 22:23:13 +0300

    Problem P-0042 Solution

---
+++ Level-1/Loops/P-Shape1-0042.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+  int x ; 
+  cin>>x;
+  do{
+    for(int i = 1 ; i<=x ; i++){
+        cout<<'*';
+    }
+    
+    cout<<endl;
+  }while(x--);
+}
