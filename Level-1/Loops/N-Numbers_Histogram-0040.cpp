cac54e92140e48fc69f51182a4f85dbd65870a6d
Adnan <adnan1262a@gmail.com>
2026-09-23 12:59:05 +0300

    Problem N-0040 Solution

---
+++ Level-1/Loops/N-Numbers_Histogram-0040.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+   char s ; 
+   int t , n ;
+   cin>>s>>t;
+   while(t--){
+    cin>>n;
+    while(n--){
+        cout<<s;
+    }
+    cout<<endl;
+   }
+    
+}
