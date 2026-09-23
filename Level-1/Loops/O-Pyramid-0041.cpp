48c1f385a53b4f76ad7196373c67e2176b4a9c0b
Adnan <adnan1262a@gmail.com>
2026-09-23 13:04:25 +0300

    Problem O-0041 Solution

---
+++ Level-1/Loops/O-Pyramid-0041.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+   int n , l;
+   cin>>n;
+   for (int i = 1 ; i<=n ; i++){
+    l=i;
+    while(l--){
+        cout<<'*';
+    }
+    cout<<endl;
+   }
+}
