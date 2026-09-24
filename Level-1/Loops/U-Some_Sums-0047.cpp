49e6172c4552562ac3158eaa75cc43b5c68c4fcd
Adnan <adnan1262a@gmail.com>
2026-09-24 17:35:36 +0300

    Problem U-0047 Solution

---
+++ Level-1/Loops/U-Some_Sums-0047.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+int n , a , b ; 
+cin>>n>>a>>b;
+long long sum = 0 ; 
+/*for (int i = min(a,b); i<= max(a,b ) ; i++){
+    sum+=i;
+   
+}*/
+for (int i = 1 ; i<=n ; i++){
+    int s = 0 , x=i , flag= 0 ;
+    while(x!=0){
+        s+=(x%10);
+        x/=10;
+        
+        
+    }for(int j = min(a,b); j<= max(a,b ) ; j++ ){
+            if(s==j)
+            flag=1;
+        }
+    if(flag)
+    sum+=i;
+
+}
+cout<<sum;
+
+}
