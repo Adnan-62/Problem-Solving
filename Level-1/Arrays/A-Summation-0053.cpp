497f66d6f9c451a3d296ae0729a3ad23a1b83f9b
Adnan <adnan1262a@gmail.com>
2026-09-25 21:22:49 +0300

    Problem A-0053 Solution

---
+++ Level-1/Arrays/A-Summation-0053.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+ 
+int main() {
+    long long  n ; cin>>n;
+    long long  arr[n] , sum = 0; 
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    if(sum<0){
+        sum*=-1;
+
+    }
+    cout<<sum;
+
+}
