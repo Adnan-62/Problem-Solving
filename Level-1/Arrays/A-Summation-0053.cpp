947b27974ca2774a79bfc257834693ba03c8b352
Adnan <adnan1262a@gmail.com>
2026-09-25 22:07:06 +0300

    Problem B-0053 Solution

--- Level-1/Arrays/A-Summation-0053.cpp
+++ Level-1/Arrays/A-Summation-0053.cpp
@@ -4,16 +4,22 @@ using namespace std;
 
  
 int main() {
-    long long  n ; cin>>n;
-    long long  arr[n] , sum = 0; 
-    for(int i = 0 ; i<n ; i++){
-        cin>>arr[i];
-        sum+=arr[i];
+ int n , x , flag=0 , j; 
+ cin>>n;
+ int arr[n];
+for(int i = 0 ; i<n ; i++){
+    cin>>arr[i];
+}
+cin>>x;
+for(int i = 0 ; i<n ;i++){
+    if(x==arr[i]){
+        flag=1 ;
+        j=i;
+        break;
     }
-    if(sum<0){
-        sum*=-1;
-
-    }
-    cout<<sum;
-
+}
+if(flag)
+cout<<j;
+else
+cout<<-1;
 }
